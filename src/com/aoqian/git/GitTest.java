ecbe9c6ec6957d013c11f3013a8a5bc996de0707
gitaoqian <909452639@qq.com>
2016-08-17 11:35:33 +0800

    首次提交到Git本地仓库

---
+++ src/com/aoqian/git/GitTest.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.aoqian.git;
+
+/**
+ * @author Administrator
+ *
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+}
